af5aaebf072363419d9283619c199110e8046cd0
Felix Grohs <contact@felixgrohs.at>
2021-03-01 16:21:04 +0100

    add event methods

--- src/CustCur.js
+++ src/CustCur.js
@@ -61,6 +61,11 @@ function CustCur (options = {}) {
 	}
 
 
+	/**
+	 * Proxy mouseover event from target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onEnter = (e) => {
 		if(!this._enabled) return;
 		this._toggleVisibility(true);
@@ -69,14 +74,41 @@ function CustCur (options = {}) {
 	}
 
 
+	/**
+	 * Listen for mouseover event on specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onEnter = (callable) => this._options.onEnter = callable;
+
+
+	/**
+	 * Proxy mouseout event from target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onLeave = (e) => {
 		if(!this._enabled) return;
+
 		this._toggleVisibility(false);
 
 		this._options.onLeave(e);
 	}
 
 
+	/**
+	 * Listen for mouseout event within target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onLeave = (callable) => this._options.onLeave = callable;
+
+
+	/**
+	 * Proxy mousemove event from target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onMove = (e) => {
 		if(!this._enabled) return;
 		this._toggleVisibility(true);
@@ -88,44 +120,116 @@ function CustCur (options = {}) {
 	}
 
 
+	/**
+	 * Listen for mousemove event within specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onMove = (callable) => this._options.onMove = callable;
+
+
+	/**
+	 * Proxy mouseover event from target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onHover = (e) => {
 		if(!this._enabled) return;
+
 		this._node.classList.add(this._options.classes.hover);
 
 		this._options.onHover(e);
 	}
 
 
+	/**
+	 * Listen for mouseover event within specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onHover = (callable) => this._options.onHover = callable;
+
+
+	/**
+	 * Proxy mouseout event from target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onUnhover = (e) => {
 		if(!this._enabled) return;
+
 		this._node.classList.remove(this._options.classes.hover);
 
 		this._options.onUnhover(e);
 	}
 
+	/**
+	 * Listen for mouseout event from specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onUnhover = (callable) => this._options.onUnhover = callable;
 
+
+	/**
+	 * Proxy mousedown events within target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onClick = (e) => {
 		if(!this._enabled) return;
+
 		this._node.classList.add(this._options.classes.click);
 
 		this._options.onClick(e);
 	}
 
 
+	/**
+	 * Listen for mousedown events withing specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onClick = (callable) => this._options.onClick = callable;
+
+
+	/**
+	 * Proxy mouseup events within target.
+	 *
+	 * @param {MouseEvent} e
+	 */
 	this._onClickRelease = (e) => {
 		if(!this._enabled) return;
+
 		this._node.classList.remove(this._options.classes.click);
 
 		this._options.onClickRelease(e);
 	}
 
 
+	/**
+	 * Listen for mouseup events withing specified target.
+	 *
+	 * @param {function} callable
+	 */
+	this.onClickRelease = (callable) => this._options.onClickRelease = callable;
+
+
+	/**
+	 * Toggle visibility of cursor.
+	 *
+	 * @param {Boolean} isVisible
+	 */
 	this._toggleVisibility = (isVisible) => {
 		this._node.style.visibility = isVisible ? 'visible' : 'hidden';
 		this._node.style.opacity = isVisible ? 1 : 0;
 	}
 
-
+	/**
+	 * Creates the cursor node if necessary and assigns
+	 * important css rules.
+	 *
+	 */
 	this._createCursor = () => {
 		this._node = this._options.node ?? document.createElement(this._options.tag);
 
