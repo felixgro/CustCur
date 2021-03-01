1a8306b99bb16d39adce9323eb4d286b44734714
Felix Grohs <contact@felixgrohs.at>
2021-03-01 15:25:37 +0100

    refactoring main class

--- src/CustCur.js
+++ src/CustCur.js
@@ -1,44 +1,68 @@
-const { merge } = require('lodash');
+const DEFAULTS = require('./defaults');
+const { merge, cloneDeep } = require('lodash');
 
 const {
-	setEventListeners,
-	hideDefaultCursor,
-	showDefaultCursor
+	toggleEventListeners,
+	toggleDefaultCursor,
+	toNodes
 } = require('./helpers');
 
 
 function CustCur (options = {}) {
 
-	this._options = merge(require('./defaults.js'), options);
 	this._enabled = false;
+	this._options = merge(cloneDeep(DEFAULTS), options);
+	this._hoverables = toNodes(this._options.hoverables, this._options.target);
 
 
+	/**
+	 * Toggle enabled state of custom cursor instance.
+	 *
+	 * @return {CustCur}
+	 */
+	this.toggle = () => this._enabled ? this.disable() : this.enable();
+
+
+	/**
+	 * Enable custom cursor instance at runtime.
+	 *
+	 * @return {CustCur}
+	 */
 	this.enable = () => {
 		if(this._enabled) return;
-
 		this._enabled = true;
-		setEventListeners(true, this);
-		hideDefaultCursor(this._options.target);
-		this._createCursor();
+
+		toggleEventListeners(this, true);
+		if (this._options.hideDefault) toggleDefaultCursor(this._options.target, false);
+
+		if(!this._node) this._createCursor();
 		this._toggleVisibility(false);
 
 		return this;
 	}
 
 
+	/**
+	 * Disable custom cursor instance at runtime.
+	 *
+	 * @return {CustCur}
+	 */
 	this.disable = () => {
 		if(!this._enabled) return;
 
 		this._enabled = false;
-		setEventListeners(false, this);
-		showDefaultCursor(this._options.target);
-		this._removeCursor();
+
+		toggleEventListeners(this, false);
+		toggleDefaultCursor(this._options.target, true);
+
+		this._toggleVisibility(false);
 
 		return this;
 	}
 
 
 	this._onEnter = (e) => {
+		if(!this._enabled) return;
 		this._toggleVisibility(true);
 
 		this._options.onEnter(e);
@@ -46,6 +70,7 @@ function CustCur (options = {}) {
 
 
 	this._onLeave = (e) => {
+		if(!this._enabled) return;
 		this._toggleVisibility(false);
 
 		this._options.onLeave(e);
@@ -54,10 +79,10 @@ function CustCur (options = {}) {
 
 	this._onMove = (e) => {
 		if(!this._enabled) return;
-
 		this._toggleVisibility(true);
-		this._el.style.top = `${e.clientY}px`;
-		this._el.style.left = `${e.clientX}px`;
+
+		this._node.style.top = `${e.clientY}px`;
+		this._node.style.left = `${e.clientX}px`;
 
 		this._options.onMove(e);
 	}
@@ -65,7 +90,7 @@ function CustCur (options = {}) {
 
 	this._onHover = (e) => {
 		if(!this._enabled) return;
-		this._el.classList.add(this._options.cursor.hoverClass);
+		this._node.classList.add(this._options.classes.hover);
 
 		this._options.onHover(e);
 	}
@@ -73,7 +98,7 @@ function CustCur (options = {}) {
 
 	this._onUnhover = (e) => {
 		if(!this._enabled) return;
-		this._el.classList.remove(this._options.cursor.hoverClass);
+		this._node.classList.remove(this._options.classes.hover);
 
 		this._options.onUnhover(e);
 	}
@@ -81,7 +106,7 @@ function CustCur (options = {}) {
 
 	this._onClick = (e) => {
 		if(!this._enabled) return;
-		this._el.classList.add(this._options.cursor.clickClass);
+		this._node.classList.add(this._options.classes.click);
 
 		this._options.onClick(e);
 	}
@@ -89,38 +114,29 @@ function CustCur (options = {}) {
 
 	this._onClickRelease = (e) => {
 		if(!this._enabled) return;
-		this._el.classList.remove(this._options.cursor.clickClass);
+		this._node.classList.remove(this._options.classes.click);
 
 		this._options.onClickRelease(e);
 	}
 
 
 	this._toggleVisibility = (isVisible) => {
-		this._el.style.visibility = isVisible ? 'visible' : 'hidden';
-		this._el.style.opacity = isVisible ? 1 : 0;
+		this._node.style.visibility = isVisible ? 'visible' : 'hidden';
+		this._node.style.opacity = isVisible ? 1 : 0;
 	}
 
 
 	this._createCursor = () => {
-		this._el = this._options.cursor.el ?? document.createElement(this._options.cursor.tag);
-		this._el.classList.add(this._options.cursor.class);
+		this._node = this._options.node ?? document.createElement(this._options.tag);
 
-		Object.assign(this._el.style, {
+		Object.assign(this._node.style, {
 			position: 'fixed',
 			'pointer-events': `none`
 		});
 
-		if (!this._options.cursor.el)
-			document.body.appendChild(this._el);
-	}
-
-
-	this._removeCursor = () => {
-		if(this._options.cursor.el) {
-			this._toggleVisibility(false);
-		} else {
-			this._el = null;
-			document.querySelector(`.${this._options.cursor.class}`).remove();
+		if (!this._options.node) {
+			this._node.classList.add(this._options.classes.base);
+			document.body.appendChild(this._node);
 		}
 	}
 
